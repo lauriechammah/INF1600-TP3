d3ab317abd56dedc8683368564edb1cbb687b410
Laurie Chammah <lachas@gigl.polymtl.ca>
2026-02-21 17:43:34 -0500

    applyScanline.s complété

--- src/crt/apply_scanline.s
+++ src/crt/apply_scanline.s
@@ -21,6 +21,39 @@ applyScanline:
     movl    %esp, %ebp   
 
     # TODO
+
+    # Arguments
+    # p = 8(%ebp)    -> adresse du pixel (Pixel)
+    # percent = 12(%ebp)
+
+    movl 8(%ebp), %esi          # esi = &p
+    movl 12(%ebp), %ecx         # ecx = percent
+
+    ####### Traitement composante R #######
+    movzbl (%esi), %eax         # on met un seul octet (R) étendu en 32 bits dans %eax (pour les prochaines opérations)
+    imull %ecx, %eax            # eax = R * percent
+    movl $0, %edx               # clean, éviter overflow
+    movl percent_conversion, %ebx
+    idivl %ebx                  # diviser eax par 100
+    movb %al, (%esi)            # nouvelle valeur de R dans pixel
+
+    ####### Traitement composante G #######
+    movzbl 1(%esi), %eax        # même logique, mais octet suivant 
+    imull %ecx, %eax            
+    movl $0, %edx               
+    movl percent_conversion, %ebx
+    idivl %ebx                  
+    movb %al, 1(%esi)        
+
+    ####### Traitement composante B #######
+    movzbl 2(%esi), %eax        # même logique, mais octet suivant 
+    imull %ecx, %eax            
+    movl $0, %edx               
+    movl percent_conversion, %ebx
+    idivl %ebx                  
+    movb %al, 2(%esi)    
+
+    ####### A est ignoré #######
     
     # epilogue
     leave 
